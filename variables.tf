11fc722fe326117a1de13ac37a6c5d0763d499ee
Brian Adams <service@contrasting.org>
2021-04-23 13:55:48 -0500

    Added initial tf code

---
+++ variables.tf
@@ -0,0 +1,54 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "key_name" {
+  default = "badams"
+}
+
+variable "bucket" {
+  default = ""
+}
+
+variable "ami_name" {
+  default = "amzn-ami-hvm*"
+}
+
+variable "ami_owner" {
+  default = "amazon"
+}
+
+variable "name" {
+  default = "foo"
+}
+
+variable "costcenter" {
+  default = "foo@example.com"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "key" {
+  default = "null.tfstate"
+}
+
+variable "asg_size" {
+  default = "t2.micro"
+}
+
+variable "phone_number" {
+  description = "Used for getting auto scale alerts"
+  default     = "+15555555555"
+}
+
+variable "whitelist" {
+  description = "IPs to whitelist for ssh access"
+  default     = ["127.0.0.1/32"]
+}
+
