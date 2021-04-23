af486376b5c1c2aedc56af57a5d3f17f4304f648
Brian Adams <service@contrasting.org>
2021-04-23 14:51:42 -0500

    Added backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "badams-foo-us-east-2"
-    key    = "dev.tf"
+    bucket = "badams-foo-us-west-2"
+    key    = "badams-dev.tf"
     region = "us-west-2"
   }
 } 
