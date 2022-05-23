63381393c7f3d3b09c01d0f946a0347bf8ac4cd2
Brian A <service@contrasting.org>
2022-05-23 16:17:09 -0500

    Update user_data.sh

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -1,7 +1,61 @@
 #!/bin/bash
 yum install docker -y 
 service docker start
-docker run -d -p 80:80 nginx:stable
+
+echo '
+version: '3'
+
+services:
+  web:
+    restart: always
+    build: ./web
+    expose:
+      - "8000"
+    links:
+      - postgres:postgres
+      - redis:redis
+    volumes:
+      - web-django:/usr/src/app
+      - web-static:/usr/src/app/static
+    env_file: .env
+    environment:
+      DEBUG: 'true'
+    command: /usr/local/bin/gunicorn docker_django.wsgi:application -w 2 -b :8000
+
+  nginx:
+    restart: always
+    build: ./nginx/
+    ports:
+      - "80:80"
+    volumes:
+      - web-static:/www/static
+    links:
+      - web:web
+
+  postgres:
+    restart: always
+    image: postgres:latest
+    ports:
+      - "5432:5432"
+    volumes:
+      - pgdata:/var/lib/postgresql/data/
+
+  redis:
+    restart: always
+    image: redis:latest
+    ports:
+      - "6379:6379"
+    volumes:
+      - redisdata:/data
+
+volumes:
+  web-django:
+  web-static:
+  pgdata:
+  redisdata:' > ./docker-compose.yaml
+
+docker-compose build 
+docker-compose up -d 
 
 printf '
 REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone  | sed -e "s/.$//")
