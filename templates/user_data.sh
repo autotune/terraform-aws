f554f1caa3677b6059970b97fc7282f08a72b1cf
Brian A <service@contrasting.org>
2022-05-23 16:49:57 -0500

    Update user_data.sh

--- templates/user_data.sh
+++ templates/user_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 yum install docker -y 
 service docker start
-echo "test"
+curl -L "https://github.com/docker/compose/releases/download/2.5.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 echo '
 version: '3'
